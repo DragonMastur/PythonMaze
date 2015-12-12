b9afa770e87e28a77626c07d7b28f4edcf57750e
DragonMastur <preston@hagerfamily.com>
2015-12-11 20:46:09 -0800

    Update mazechecker.py

--- mazechecker.py
+++ mazechecker.py
@@ -1,2 +1,6 @@
+import pepper
+
 def checkifpossible(gb):
+	# The gb is a matrix like this [[G,G,G,G,G,G,G,G],[G,G,G,G,G,G,G,G]... or
+	#	[[pepper.GameSquare() for x in range(8)] for y in range(8)]
 	return True
