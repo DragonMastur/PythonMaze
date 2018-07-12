3d1f266ab0fcc5a0bbcb12288865ff1d0bc2fe93
Preston Hager <preston@hagerfamily.com>
2018-07-12 14:22:41 -0700

    Fixed all misspellings of "postion" to "position".

--- pepper.py
+++ pepper.py
@@ -9,13 +9,13 @@ try:
 	import tkinter as tk
 except:
 	import Tkinter as tk
-	
+
 class GameSquare:
 	def __init__(self, color='none', outer='none', text=''):
 		self.color = color
 		self.outer = outer
 		self.text = text
-	
+
 	def setnew(self, color=None, outer=None, text=None):
 		'''
 		Set new value to color, outer or text.
@@ -26,8 +26,8 @@ class GameSquare:
 			self.outer = outer
 		if text != None:
 			self.text = text
-	
-	
+
+
 class d2GameScreen:
 	def c_rect(self, x1, y1, x2, y2, color="white", outer="black", text=""):
 		'''
@@ -35,7 +35,7 @@ class d2GameScreen:
 		'''
 		self.can.create_rectangle(x1, y1, x2, y2, fill=color, outline=outer)
 		self.can.create_text((x2-x1)/2+x1, (y2-y1)/2+y1, text=text)
-	
+
 	def update(self):
 		'''
 		Call this to update the gamescreen
@@ -47,7 +47,7 @@ class d2GameScreen:
 					self.c_rect(i*self.rectw, j*self.recth, (i+1)*self.rectw, (j+1)*self.recth, self.gameboard[i][j].color, self.gameboard[i][j].outer, self.gameboard[i][j].text)
 				if self.gameboard[i][j].color != 'none' and self.gameboard[i][j].outer == 'none':
 					self.c_rect(i*self.rectw, j*self.recth, (i+1)*self.rectw, (j+1)*self.recth, self.gameboard[i][j].color, text=self.gameboard[i][j].text)
-		
+
 	def updateWhile(self):
 		'''
 		Call this to contunuesly call update.
@@ -56,7 +56,7 @@ class d2GameScreen:
 		while self.quitnow != True:
 			time.sleep(0.001)
 			self.update()
-	
+
 	def setsquare(self, x, y, square):
 		'''
 		Set square of x, y to value of square.
@@ -66,14 +66,14 @@ class d2GameScreen:
 			self.gameboard[x][y] = square
 		except:
 			raise IndexError("The x and/or y value is out of range.")
-	
+
 	def clear(self, blocks):
 		'''
-		Clear gameboard with blocks, blocks is a GameSquare. 
-		Does not update screen. 
+		Clear gameboard with blocks, blocks is a GameSquare.
+		Does not update screen.
 		'''
 		self.gameboard = [[blocks for x in range(int(self.rootgeo[0]/self.rectw))] for y in range(int(self.rootgeo[1]/self.recth))]
-	
+
 	def __init__(self, gb='', roottitle="2D GameScreen", rootgeo=(500, 500), canbgcolor="white", xview=20, yview=20):
 		'''
 		gb -			The gameboard, leave '' to generate gameboard with defualt GameSquare.
@@ -97,7 +97,7 @@ class d2GameScreen:
 		self.root = tk.Tk()
 		self.root.title(roottitle)
 		self.root.geometry(rootgeostr)
-		
+
 		self.can = tk.Canvas(self.root, width=rootgeo[0], height=rootgeo[1], bg=canbgcolor)
 		self.can.pack()
 		self.update()
@@ -116,30 +116,30 @@ class GameScreen:
 		self.root = tk.Tk()
 		self.root.title(roottitle)
 		self.root.geometry(str(rootgeo[0])+'x'+str(rootgeo[1]))
-		
+
 		self.canvas = tk.Canvas(self.root, width=rootgeo[0], height=rootgeo[1], bg='white')
 		self.canvas.pack()
-	
+
 	def setgameboard(self, gb):
 		self.gameboard = gb
-	
+
 	def rotatecamera(self, degrees, dir='x'):
 		directions = ['x','y','z']
 		self.camerarotationdegrees[direction.index(dir)] += degrees
-	
+
 	def clear(self):
 		self.canvas.delete(tk.ALL)
 		self.gameboard = [[[GameSquare() for x in range(xview)] for y in range(yview)] for z in range(zview)]
-	
+
 	def c_square(self, x1, y1, x2, y2, depth, fill='white', outer='black'):
 		self.canvas.create_polygon(x1, y1, x2, y2, fill=fill, outline=outer)
-		# calculate the z1 and z2 postions
+		# calculate the z1 and z2 positions
 		z1 = depth
 		z2 = depth
 		self.canvas.create_polygon(x1, z1, x2, z2, fill=fill, outline=outer)
 		self.canvas.create_polygon(y1, z1, y2, z2, fill=fill, outline=outer)
 		# I might need to add more...
-	
+
 	def update(self):
 		self.clear()
 		for i in range(len(self.gameboard)):
@@ -161,26 +161,26 @@ class GameScreen:
 									  self.camerarotationdegrees,
 									  fill=self.gameboard[i][j][k].color)
 						print(str(i)+', '+str(j)+', '+str(k))
-	
+
 	def setpoint(self, x, y, z, block):
 		self.gameboard[x][y][z] = block
-	
+
 	def c_rect(self, x1, y1, x2, y2, color='white', outer='', text=''):
 		self.can.create_rectangle(x1, y1, x2, y2, fill=color, outline=outer)
 		self.can.create_text(x1, y1, x2, y2, text=text)
-		
+
 
 class Game:
 	def __init__(self, dem=3, poscolor='black', pos=[0,0], xaxis=20, yaxis=20, zaxis=20, geo=(500,500), title="Pepper Game", gb=None):
 		self.poscolor = poscolor
 		self.dem = dem
-		self.postion = pos
+		self.position = pos
 		if dem == 3:
 			if gb == None:
 				gb = [[[GameSquare() for x in range(xaxis)] for y in range(yaxis)] for z in range(zaxis)]
 			self.GS = GameScreen(xview=xaxis, yview=yaxis, zview=zxis, rootgeo=geo, roottitle=title, gb=gb)
 			self.GS.gameboard[4][4][4].setnew(color='red', outer='black')
-			self.postion.append(0)
+			self.position.append(0)
 		if dem == 2:
 			if gb == None:
 				gb = [[GameSquare() for x in range(xaxis)] for y in range(yaxis)]
@@ -192,7 +192,7 @@ class Game:
 		self.update()
 		self.GS.root.bind("<Key>", self.keyPressed)
 		self.GS.update()
-		
+
 	def keyPressed(self, event):
 		if event.char == 'w':
 			if self.dem == 3:
@@ -217,33 +217,33 @@ class Game:
 		if event.char == ' ':
 			if self.dem == 3:
 				self.move(0,0,1)
-		
+
 	def update(self):
 		self.GS.update()
 		if self.dem == 2:
-			if self.GS.gameboard[self.postion[0]][self.postion[1]].outer != 'none':
-				self.GS.c_rect(self.postion[0]*self.GS.rectw, self.postion[1]*self.GS.recth, (self.postion[0]+1)*self.GS.rectw, (self.postion[1]+1)*self.GS.recth, self.poscolor, self.GS.gameboard[self.postion[0]][self.postion[1]].outer, self.GS.gameboard[self.postion[0]][self.postion[1]].text)
+			if self.GS.gameboard[self.position[0]][self.position[1]].outer != 'none':
+				self.GS.c_rect(self.position[0]*self.GS.rectw, self.position[1]*self.GS.recth, (self.position[0]+1)*self.GS.rectw, (self.position[1]+1)*self.GS.recth, self.poscolor, self.GS.gameboard[self.position[0]][self.position[1]].outer, self.GS.gameboard[self.position[0]][self.position[1]].text)
 			else:
-				self.GS.c_rect(self.postion[0]*self.GS.rectw, self.postion[1]*self.GS.recth, (self.postion[0]+1)*self.GS.rectw, (self.postion[1]+1)*self.GS.recth, self.poscolor, text=self.GS.gameboard[self.postion[0]][self.postion[1]].text)
+				self.GS.c_rect(self.position[0]*self.GS.rectw, self.position[1]*self.GS.recth, (self.position[0]+1)*self.GS.rectw, (self.position[1]+1)*self.GS.recth, self.poscolor, text=self.GS.gameboard[self.position[0]][self.position[1]].text)
 		else:
 			raise VersionError("3D mapping not avalable yet. Sorry")
 		try:
 			self.onUpdateExc()
 		except:
 			pass
-		
+
 	def checkPos(self):
-		if self.postion[0]<0: self.postion[0]=0;
-		if self.postion[1]<0: self.postion[1]=0;
-		if self.postion[0]>int(self.GS.rootgeo[0]/self.GS.rectw)-1: self.postion[0]=int(self.GS.rootgeo[0]/self.GS.rectw)-1;
-		if self.postion[1]>int(self.GS.rootgeo[1]/self.GS.recth)-1: self.postion[1]=int(self.GS.rootgeo[1]/self.GS.recth)-1;
-		
+		if self.position[0]<0: self.position[0]=0;
+		if self.position[1]<0: self.position[1]=0;
+		if self.position[0]>int(self.GS.rootgeo[0]/self.GS.rectw)-1: self.position[0]=int(self.GS.rootgeo[0]/self.GS.rectw)-1;
+		if self.position[1]>int(self.GS.rootgeo[1]/self.GS.recth)-1: self.position[1]=int(self.GS.rootgeo[1]/self.GS.recth)-1;
+
 	def move(self, x, y, z=None):
 		try:
-			self.postion[0] += x
-			self.postion[1] += y
+			self.position[0] += x
+			self.position[1] += y
 			if z != None:
-				self.postion[2] += z
+				self.position[2] += z
 				self.onMoveExc(x,y,z)
 			else:
 				self.onMoveExc(x,y)
@@ -251,7 +251,7 @@ class Game:
 			pass
 		self.checkPos()
 		self.update()
-		
+
 
 d3GameScreen = GameScreen
 
